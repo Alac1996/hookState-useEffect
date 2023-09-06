bec5d7a6861bb8b737e33cb08a3ff6f97646b245
Alac1996 <alacarno@gmail.com>
2023-09-06 15:09:28 +0700

    Movie-app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// step-1 : เก็บ Input จาก User => React State
+// step-2 : handle จังหวะ submit
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [keyword, setKeyword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+
+  return (
+    <div className="App">
+      {/* {header} */}
+      <div className="header">
+        <h1>Movie App</h1>
+      </div>
+
+      {/* {Input} */}
+      <form className="Search" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="keyword ?"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {/* {Result} */}
+      <div className="movie-lists">
+        <div className="movie">Movie-1</div>
+        <div className="movie">Movie-2</div>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
